da5548814336a2441dc21ea09cedc00bd9ef8400
joezelle <51567424+joezelle@users.noreply.github.com>
2020-02-25 01:16:24 +0100

    svelte basics

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,51 @@
 <script>
-	export let name;
+  import ContactCard from "./ContactCard.svelte";
+  let name = "";
+  let jobTitle = "";
+  let description = "";
+  let image = "";
+  $: uppercaseName = name.toUpperCase();
+
+  function changeName() {
+    name = "John";
+  }
+
+  // using a function
+  function inputName(event) {
+    const enteredValue = event.target.value;
+    name = enteredValue;
+  }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<h1>Hello {name}</h1>
+<!-- <button on:click={changeName}>Change Name</button> -->
+<!-- <input type="text" value={name} on:input={inputName} /> -->
+<input type="text" bind:value={name} />
+<input type="text" bind:value={image} />
+<input type="text" bind:value={jobTitle} />
+<input type="text" bind:value={description} />
+
+<ContactCard username={name} {jobTitle} {description} {image} />
+<main />
