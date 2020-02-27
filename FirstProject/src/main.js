19125d2c22c202c4476cce79d3006b81f0776c67
joezelle <51567424+joezelle@users.noreply.github.com>
2020-02-27 22:41:45 +0100

    mounting components

--- FirstProject/src/main.js
+++ FirstProject/src/main.js
@@ -1,7 +1,15 @@
-import App from './App.svelte';
+import App from "./App.svelte";
+// import Header from './UI/Header.svelte';
 
 const app = new App({
-	target: document.body
+  // target: document.querySelector('#app')
+  target: document.body
 });
+
+// const header = new Header({
+// target:document.querySelector('#header');
+// })
+
+//this kind of mounting of components will not allow components talk to each other like it would in SPA, this is because they are mounted in the html page separately.
 
 export default app;
