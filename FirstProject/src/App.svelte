bc863c2f11ca6e80ca20e22342670a73c1f0a252
joezelle <51567424+joezelle@users.noreply.github.com>
2020-02-28 00:37:04 +0100

    adding grid

--- FirstProject/src/App.svelte
+++ FirstProject/src/App.svelte
@@ -1,5 +1,97 @@
 <script>
   import Header from "./UI/Header.svelte";
+  import MeetupGrid from "./Meetups/MeetupGrid.svelte";
+
+  let title = "";
+  let subtitle = "";
+  let description = "";
+  let email = "";
+  let address = "";
+  let imageUrl = "";
+
+  let meetups = [
+    {
+      id: "meet1",
+      title: "Coding Bootcamp",
+      subtitle: "Learn to code in 30 Minutes",
+      description: "In this meetup, we will have some experts that teach",
+      imageUrl:
+        "https://elevenfifty.org/wp-content/uploads/2017/04/Coding-Bootcamp-1-880x499.jpg",
+      address: "27the Nerd Road,  32523 New York",
+      contactEmail: "testing-testing@eamail.com"
+    },
+    {
+      id: "meet2",
+      title: "Boys Night Out",
+      subtitle: "Boys Stepping Out",
+      description: "Time for the real Gees to have some good time, bro-bonding",
+      imageUrl:
+        "https://www.partner-kz.com/wp-content/uploads/2019/01/Great-Boys%E2%80%99-Night-Out.jpg",
+      address: "1020 ParkEast Road,  32523 Las Vegas",
+      contactEmail: "testing@eamail.com"
+    },
+    {
+      id: "meet3",
+      title: "Double Date",
+      subtitle: "First of its Kind",
+      description: "Its time for that double date amongs the lovers",
+      imageUrl:
+        "https://www.liberaldictionary.com/wp-content/uploads/2018/12/double-date.jpg",
+      address: "2234 Starklet Road,  32523 Los Angeles",
+      contactEmail: "testing-testing@eamail.com"
+    }
+  ];
+
+  function addMeetup() {
+    const newMeetup = {
+      id: Math.random().toString(),
+      title: title,
+      subtitle: subtitle,
+      description: description,
+      imageUrl: imageUrl,
+      address: address,
+      contactEmail: email
+    };
+
+    meetups = [newMeetup, ...meetups];
+  }
 </script>
 
+<style>
+  main {
+    margin-top: 5rem;
+  }
+</style>
+
 <Header />
+
+<main>
+  <form on:submit|preventDefault={addMeetup}>
+    <div class="form-control">
+      <label for="title">Title</label>
+      <input type="text" id="title" bind:value={title} />
+    </div>
+    <div class="form-control">
+      <label for="subtitle">Subtitle</label>
+      <input type="text" id="subtitle" bind:value={subtitle} />
+    </div>
+    <div class="form-control">
+      <label for="address">Address</label>
+      <input type="text" id="address" bind:value={address} />
+    </div>
+    <div class="form-control">
+      <label for="image">Image Url</label>
+      <input type="text" id="imageUrl" bind:value={imageUrl} />
+    </div>
+    <div class="form-control">
+      <label for="email">Email</label>
+      <input type="email" id="email" bind:value={email} />
+    </div>
+    <div class="form-control">
+      <label for="description">Description</label>
+      <textarea rows="3" id="description" bind:value={description} />
+    </div>
+    <button type="submit">Save</button>
+  </form>
+  <MeetupGrid {meetups} />
+</main>
