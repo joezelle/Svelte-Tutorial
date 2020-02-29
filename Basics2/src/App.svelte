bf44c8fa982f6920e50b1223f5fa4fa78231baf5
joezelle <51567424+joezelle@users.noreply.github.com>
2020-02-29 16:09:00 +0100

    using the spread operator

--- Basics2/src/App.svelte
+++ Basics2/src/App.svelte
@@ -1,12 +1,44 @@
 <script>
   import Product from "./Product.svelte";
+
+  let products = [
+    {
+      id: "p1",
+      title: "Shoe",
+      price: 9.99
+    },
+    {
+      id: "p2",
+      title: "Bag",
+      price: 4.99,
+      onDemand: "On Demand"
+    }
+  ];
+
+  function addToCart(event) {
+    console.log(event);
+  }
+
+  function deleteProduct(event) {
+    console.log(event);
+  }
 </script>
 
 <main>
+  <!-- this is one way of passing props -->
+  <!-- {#each products as product}
+    <Product 
+      title={product.title}
+      price={product.price}
+      on:add-to-cart={addToCart}
+      on:delete={deleteProduct} />
+  {/each} -->
 
-  <Product
-    productTitle="Shoes"
-    on:add-to-cart={() => alert('add to cart')}
-    on:delete={() => alert('delete')} />
-
+  <!-- using the spread operator as long as props match the variable name-->
+  {#each products as product}
+    <Product
+      {...product}
+      on:add-to-cart={addToCart}
+      on:delete={deleteProduct} />
+  {/each}
 </main>
--- Basics2/src/Product.svelte
+++ Basics2/src/Product.svelte
@@ -1,17 +1,28 @@
 <script>
   import { createEventDispatcher } from "svelte";
 
-  export let productTitle;
+  export let title;
+  export let price;
+  export let id;
+  export let onDemand = false;
 
   const dispatch = createEventDispatcher();
 
   function addToCart() {
     dispatch("add-to-cart", { id: "p1" });
   }
+  function deleteCart() {
+    dispatch("delete", { id: "p1" });
+  }
 </script>
 
 <article>
-  <h1>{productTitle}</h1>
+  <h1>{title}</h1>
+  <h3>${price}</h3>
+
+  {#if onDemand}
+    <h3>{onDemand}</h3>
+  {/if}
   <button on:click={addToCart}>Add to cart</button>
   <button
     on:click={() => {
