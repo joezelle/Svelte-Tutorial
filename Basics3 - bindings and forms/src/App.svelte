cb34ddfb2f154f0c2067feb12f34348c8acd1930
joezelle <51567424+joezelle@users.noreply.github.com>
2020-03-04 23:40:04 +0100

    binding on radio, checkbox, dropdown

--- Basics3 - bindings and forms/src/App.svelte
+++ Basics3 - bindings and forms/src/App.svelte
@@ -1,30 +1,94 @@
 <script>
-	export let name;
+  import CustomInput from "./CustomInput.svelte";
+  import Toggle from "./Toggle.svelte";
+
+  let name = "";
+  let selectedOption = 1;
+  let price = 0;
+  let agreed;
+  let favColor = "blue";
+  let selectedItem;
+  //   for checkboxes
+  //let favColor = ["blue"];
+
+  $: console.log(selectedOption);
+  $: console.log(price);
+  $: console.log(agreed);
+  $: console.log(favColor);
+  $: console.log(selectedItem);
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <CustomInput bind:val={name} />
+  <p>{name}</p>
+
+  <Toggle bind:chosenOption={selectedOption} />
+
+  <!-- to get numbers as output not string -->
+  <input type="number" bind:value={price} />
+  <!-- two way binding doesn't allow dynamic value of type -->
+
+  <!-- use bind:checked -->
+  <label>
+    <input type="checkbox" bind:checked={agreed} />
+    I agree to terms and conditions
+  </label>
+
+  <!-- for radio -->
+  Favorite Color
+  <label>
+    <input type="radio" value="blue" name={favColor} bind:group={favColor} />
+    Blue
+  </label>
+  <label>
+    <input type="radio" value="white" name={favColor} bind:group={favColor} />
+    White
+  </label>
+  <label>
+    <input type="radio" value="black" name={favColor} bind:group={favColor} />
+    Black
+  </label>
+  <label>
+    <input type="radio" value="cobalt" name={favColor} bind:group={favColor} />
+    Cobalt
+  </label>
+
+  <!-- returns an array -->
+  <label>
+    <input type="checkbox" value="blue" name={favColor} bind:group={favColor} />
+    Blue
+  </label>
+  <label>
+    <input
+      type="checkbox"
+      value="white"
+      name={favColor}
+      bind:group={favColor} />
+    White
+  </label>
+  <label>
+    <input
+      type="checkbox"
+      value="black"
+      name={favColor}
+      bind:group={favColor} />
+    Black
+  </label>
+  <label>
+    <input
+      type="checkbox"
+      value="cobalt"
+      name={favColor}
+      bind:group={favColor} />
+    Cobalt
+  </label>
+
+  <!-- Dropdown -->
+
+  <select bind:value={selectedItem} id="">
+    <option value="blue">Blue</option>
+    <option value="cobalt">cobalt</option>
+    <option value="Sapphire">Sapphire</option>
+  </select>
+
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
---
+++ Basics3 - bindings and forms/src/Toggle.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let chosenOption = 1;
+</script>
+
+<button disabled={chosenOption === 1} on:click={() => (chosenOption = 1)}>
+  button 1
+</button>
+<button disabled={chosenOption === 2} on:click={() => (chosenOption = 2)}>
+  button 2
+</button>
